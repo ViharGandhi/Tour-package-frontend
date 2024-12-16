d0ce57a53d64ec86de260bc2a9b5ab0a56a4fb3f
ViharGandhi <viharworks247@gmaiil.com>
2024-12-16 14:05:44 +0530

    Port updated for render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0',  // Allow external access (important for Render)
+    port: 10000,       // Use port 10000 as expected by Render
+  },
+});
